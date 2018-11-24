3ac794cf71b4ae7b6a25811f3434aa8fa6c9201f
Arthur Houdebine <arthur.houdebine@gmail.com>
2018-11-24 13:50:26 +0100

    Random-beer page not rendering a random beer

--- starter_code/app.js
+++ starter_code/app.js
@@ -9,13 +9,33 @@ const punkAPI = new PunkAPIWrapper();
 app.set('view engine', 'hbs');
 app.set('views', __dirname + '/views');
 app.use(express.static(path.join(__dirname, 'public')));
-
-
+hbs.registerPartials(__dirname + '/views/partials');
 
 app.get('/', (req, res, next) => {
   res.render('index');
 });
 
+app.get('/beers', (req, res, next) => {
+  punkAPI.getBeers()
+  .then(beers => {
+    res.render('beers',{beers:beers});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
+app.get('/random-beer', (req, res, next) => {
+  punkAPI.getRandom()
+  .then(beer => {
+  res.render('random-beer',{beer});
+  })
+  .catch(error => {
+    console.log(error)
+  })
+  
+});
 
 app.listen(3000);
+console.log("now running on http://localhost:3000");
